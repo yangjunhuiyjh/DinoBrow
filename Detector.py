02a1a5d5533ae683711059697521d64c787a3306
Junhui Yang <yangjunhuiyjh@gmail.com>
2020-02-15 21:27:34 +0000

    public method for average facial and eyebrow height

--- Detector.py
+++ Detector.py
@@ -36,17 +36,19 @@ class Detector():
             # convert the landmark (x, y)-coordinates to a NumPy array
             shape = self.predictor(image, rect)
             for i in range(0, 68):
-                self.coordinates[i] = (shape.part(i).x, shape.part(i).y)
+                self.coordinates[i] = (shape.part(i).x/300, shape.part(i).y/300)
 
     def getKeyPoints(self):
         print("getter", self.coordinates)
         return self.coordinates
 
     def getAvePoints(self):
-
-        return ( , )
+        face_ave_height = np.mean(self.coordinates, axis = 0)[1]
+        brow_ave_height = np.mean(self.coordinates[18:27], axis = 0)[1]
+        print(face_ave_height, brow_ave_height)
+        return (face_ave_height, brow_ave_height)
 
 if __name__ == "__main__":
     detector = Detector()
     while True:
-        detector.getKeyPoints()
+        detector.getAvePoints()
